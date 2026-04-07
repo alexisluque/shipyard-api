1de75eecab9af0bf9e456bc67ecb86b517d5045d
Alexis Luque <aleluque99@gmail.com>
2026-04-07 19:05:29 -0300

    Initial commit
    	* Add express
    	* Configure typescript
    	* Configure prettier
    	* configure ESlint

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World' });
+});
+
+app.listen(port, () => {
+  console.log('App listening on port', port);
+});
