800b91f9bcc25f7baf4f99f7c57cdd26972cf11c
Alexis Luque <aleluque99@gmail.com>
2026-04-08 13:05:30 -0300

    Set up database
        * Add pg
        * Create pool db connection
        * Create query db function
        * Add init sql script
        * Add dotenv and get variables from env
        * Add docker compose to contenirize the db

---
+++ src/db/index.ts
@@ -0,0 +1,16 @@
+import { Pool } from 'pg';
+import { env } from '../../config/env.js';
+
+const { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } = env.db;
+
+const pool = new Pool({
+  host: DB_HOST,
+  database: DB_NAME,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+});
+
+export const query = (text: string, params: string[]) => {
+  return pool.query(text, params);
+};
