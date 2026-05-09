3d4dc87495fce2016470e973b414933d74e7f1a7
Alexis Luque <aleluque99@gmail.com>
2026-05-08 21:34:52 -0300

    chore: gracefully shutdown server and db

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { createApp } from './app/app.js';
 import 'reflect-metadata';
 import { createAppDataSource } from './db/data-source.js';
 import { createLogger } from './lib/logger.js';
+import { createServer } from 'node:http';
 
 const main = async () => {
   if (!process.env.DATABASE_URL) {
@@ -17,9 +18,35 @@ const main = async () => {
 
   const app = await createApp({ db, logger });
 
-  app.listen(process.env.PORT, () => {
+  const server = createServer(app);
+
+  server.listen(process.env.PORT, () => {
     logger.info(`App listening on port: ${process.env.PORT}`);
   });
+
+  async function shutdown(signal: string) {
+    logger.info(`${signal} received, shutting down gracefully`);
+
+    server.close(async () => {
+      logger.info('HTTP server closed');
+
+      await db.destroy();
+
+      logger.info('Database connection closed');
+
+      process.exit(0);
+    });
+
+    // force shutdown after timeout
+    setTimeout(() => {
+      logger.error('Forced shutdown');
+
+      process.exit(1);
+    }, 10000).unref();
+  }
+
+  process.on('SIGINT', () => void shutdown('SIGINT'));
+  process.on('SIGTERM', () => void shutdown('SIGTERM'));
 };
 
 main().catch((error) => console.error(error));
